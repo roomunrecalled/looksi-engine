c7d1d18a6c58349882dff6a024b53b704e6395ba
M. Y. Horng <yhorng@zoho.com>
2022-09-17 03:00:11 +0000

    repository setup

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/index.js'),
+      name: 'LooksiEngine',
+      // the proper extensions will be added
+      fileName: 'looksi-engine'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
+
+})
